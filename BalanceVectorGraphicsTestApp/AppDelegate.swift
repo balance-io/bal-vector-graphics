571c52c702f2cc294c77b83410fbc30ef28304cc
Ben Baron <ben@einsteinx2.com>
2017-01-25 19:18:27 -0800

    Added sync circle to demo app

--- BalanceVectorGraphicsTestApp/AppDelegate.swift
+++ BalanceVectorGraphicsTestApp/AppDelegate.swift
@@ -14,27 +14,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    let syncButton = SyncButton(frame: CGRect(x: 100, y: 100, width: 26, height: 26))
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        if let headerView = InstitutionHeaders.headerViewForId(sourceInstitutionId: "chase") {
-            headerView.frame.origin.y = 10
-            self.window!.contentView!.addSubview(headerView)
-        }
+//        if let headerView = InstitutionHeaders.headerViewForId(sourceInstitutionId: "chase") {
+//            headerView.frame.origin.y = 10
+//            self.window!.contentView!.addSubview(headerView)
+//        }
+//        
+//        if let headerView = InstitutionHeaders.headerViewForId(sourceInstitutionId: "bofa") {
+//            headerView.frame.origin.y = 300
+//            self.window!.contentView!.addSubview(headerView)
+//        }
+//        
+//        if let headerView = InstitutionHeaders.defaultHeaderView(backgroundColor: NSColor.red, foregroundColor: NSColor.white, font: NSFont.systemFont(ofSize: 11, weight: NSFontWeightMedium), name: "Simple") {
+//            headerView.frame.origin.y = 100
+//            self.window!.contentView!.addSubview(headerView)
+//        }
+//        
+//        if let headerView = InstitutionHeaders.defaultHeaderView(backgroundColor: NSColor(deviceRed: 0.243, green: 0.278, blue: 0.298, alpha: 1), foregroundColor: NSColor.white, font: NSFont.systemFont(ofSize: 11, weight: NSFontWeightSemibold), name: "Bank of America With A Really Really Long Name That Should Clip Because I Made It Way Too Long") {
+//            headerView.frame.origin.y = 200
+//            self.window!.contentView!.addSubview(headerView)
+//        }
         
-        if let headerView = InstitutionHeaders.headerViewForId(sourceInstitutionId: "bofa") {
-            headerView.frame.origin.y = 300
-            self.window!.contentView!.addSubview(headerView)
-        }
-        
-        if let headerView = InstitutionHeaders.defaultHeaderView(backgroundColor: NSColor.red, foregroundColor: NSColor.white, font: NSFont.systemFont(ofSize: 11, weight: NSFontWeightMedium), name: "Simple") {
-            headerView.frame.origin.y = 100
-            self.window!.contentView!.addSubview(headerView)
-        }
-        
-        if let headerView = InstitutionHeaders.defaultHeaderView(backgroundColor: NSColor(deviceRed: 0.243, green: 0.278, blue: 0.298, alpha: 1), foregroundColor: NSColor.white, font: NSFont.systemFont(ofSize: 11, weight: NSFontWeightSemibold), name: "Bank of America With A Really Really Long Name That Should Clip Because I Made It Way Too Long") {
-            headerView.frame.origin.y = 200
-            self.window!.contentView!.addSubview(headerView)
-        }
+        self.window!.contentView!.addSubview(syncButton)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
